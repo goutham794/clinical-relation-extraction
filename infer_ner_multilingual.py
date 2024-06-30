923fe3f4885e8266ae1c342871d1fed6b4a10e04
goutham794 <goutham794@gmail.com>
2024-06-30 11:04:06 +0000

    feat: multilingual ner inference

---
+++ infer_ner_multilingual.py
@@ -0,0 +1,69 @@
+from simpletransformers.ner import NERModel,NERArgs
+import argparse
+import wandb
+import logging
+from seqeval.metrics import classification_report
+import secrets
+import wandb
+
+from config import Config
+import utils
+
+logging.basicConfig(level=logging.DEBUG, 
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    handlers=[logging.StreamHandler()])
+
+def configure_wandb(args):
+    wandb.login(key=args.wandb_key)
+    wandb.init(
+    project=args.wandb_proj_name,
+        name = f"{args.model}_{secrets.token_hex(2)}"
+        )
+
+def Infer_NER(args):
+
+    logging.info(f"Inference with multilingual NER model {args.model} model on {args.split} split")
+
+    model_config = args.config.model_args_ner 
+
+    model_args = NERArgs()
+    model_args.update_from_dict(model_config)
+
+    custom_labels = args.config.NER_CLASSES
+
+    model = NERModel(
+        args.model_type, 
+        f"models_multilingual/{args.model}_ner{'_combined' if args.split == 'test' else ''}",
+          args=model_args, labels=custom_labels
+    )
+
+    tokens = utils.create_tokens_list_from_file(f"data_multilingual/{args.split}_tokens.txt")
+    predictions, _ = model.predict(tokens, split_on_space=False)
+    predictions = [[list(d.values())[0] for d in i] for i in predictions]
+    utils.save_predictions_to_file(predictions, "multilingual", f'preds_{args.model}_multilingual_{args.split}_ner.txt')
+    true_entities = utils.get_true_entity_labels("multilingual", args.split)
+    if args.split == 'test':
+        metrics = classification_report(true_entities, predictions, output_dict=True)['weighted avg']
+        wandb.log({"ner_precision": metrics['precision']})
+        wandb.log({"ner_recall": metrics['recall']})
+        wandb.log({"ner_f1_score": metrics['f1-score']})
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', '-m', default='mbert')
+    parser.add_argument('--split', '-s', default='valid')
+
+    args = parser.parse_args()
+    assert args.model in ['mbert', 'xlmroberta', 'biobert','bert'], "The model must be one of bert, xlmroberta, biobert"
+    configs = Config("multilingual")
+    args.config = configs
+    args.model_type = configs.pretrained_model_details[args.model][0]
+    args.model_name = configs.pretrained_model_details[args.model][1]
+
+    args.wandb_key = configs.WANDB_API_KEY
+    args.wandb_proj_name = f"Infer_NER_multilingual_Relation_Extraction"
+    configure_wandb(args)
+
+    Infer_NER(args)
